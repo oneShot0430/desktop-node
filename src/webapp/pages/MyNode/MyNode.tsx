48e7d8516eb3dd212373f190ce25f1d72394f7f0
kjesien <krzysztofjesien@gmail.com>
2022-06-24 23:07:28 +0200

    Old table display with new modal trigger

--- src/webapp/pages/MyNode/MyNode.tsx
+++ src/webapp/pages/MyNode/MyNode.tsx
@@ -1,49 +1,43 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 
-import { showModal } from 'webapp/store/actions/modal';
+// import { showModal } from 'webapp/store/actions/modal';
 
 import MyNodeTasksTable from './components/MyNodeTasksTable';
-import MyNodeToolbar from './components/MyNodeToolbar';
+// import MyNodeToolbar from './components/MyNodeToolbar';
 
 const MyNode = (): JSX.Element => {
-  const tableHeaders = [
-    'Start/Stop',
-    'TaskName & Start Time',
-    'Role',
-    'Creator',
-    'Earned',
-    'Stake',
-    'Status',
-    'Add/Withdraw',
-  ];
-  const dispatch = useDispatch();
+  // const tableHeaders = [
+  //   'Start/Stop',
+  //   'TaskName & Start Time',
+  //   'Role',
+  //   'Creator',
+  //   'Earned',
+  //   'Stake',
+  //   'Status',
+  //   'Add/Withdraw',
+  // ];
+  // const dispatch = useDispatch();
 
   return (
     <div className="relative overflow-x-auto">
-      <table className="w-full text-[14px] text-left table-auto">
-        <thead className="pb-8 font-semibold">
-          <tr className="border-b-2">
-            {tableHeaders.map((headerText) => (
-              <th
-                key={headerText}
-                className="font-semibold leading-5 text-white racking-[0.03em] pb-4"
-              >
-                {headerText}
-              </th>
-            ))}
-          </tr>
-        </thead>
-      </table>
-      <button
-        onClick={() => dispatch(showModal('EDIT_STAKE_AMOUNT'))}
-        className="text-white"
-      >
-        Edit Stake Amount
-      </button>
-      {/* <div className="mt-34.5">
+      {/*<table className="w-full text-[14px] text-left table-auto">*/}
+      {/*  <thead className="pb-8 font-semibold">*/}
+      {/*    <tr className="border-b-2">*/}
+      {/*      {tableHeaders.map((headerText) => (*/}
+      {/*        <th*/}
+      {/*          key={headerText}*/}
+      {/*          className="font-semibold leading-5 text-white racking-[0.03em] pb-4"*/}
+      {/*        >*/}
+      {/*          {headerText}*/}
+      {/*        </th>*/}
+      {/*      ))}*/}
+      {/*    </tr>*/}
+      {/*  </thead>*/}
+      {/*</table>*/}
+      <div className="mt-34.5">
         <MyNodeTasksTable />
-      </div> */}
+      </div>
     </div>
   );
 };
