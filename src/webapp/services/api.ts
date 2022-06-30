e4926a45c4f065f2d090faecb285b35ab4efc57e
kjesien <krzysztofjesien@gmail.com>
2022-06-30 12:21:53 +0200

    More logging

--- src/webapp/services/api.ts
+++ src/webapp/services/api.ts
@@ -1,8 +1,7 @@
 import { LAMPORTS_PER_SOL, PublicKey } from '@_koi/web3.js';
 
 import sdk from 'services/sdk';
-
-import { Task } from '../@type/task';
+import { Task } from 'webapp/@type/task';
 
 import { TaskService } from './taskService';
 
@@ -16,23 +15,29 @@ export const getRewardEarned = async (
   publicKey: string,
   availableBalances: Record<string, number>
 ): Promise<number> => {
-  console.log('GETTING REWARD', publicKey);
-  const results = await window.main.getEarnedRewardByNode({
+  const result = await window.main.getEarnedRewardByNode({
     taskAccountPubKey: publicKey,
     available_balances: availableBalances,
   });
-  return results || 0;
+  console.log('GETTING REWARD', result, publicKey);
+  return result || 0;
 };
 
 export const getMainAccountBalance = (): Promise<number> => {
   return window.main
     .getMainAccountPubKey()
     .then((pubkey) => sdk.k2Connection.getBalance(new PublicKey(pubkey)))
-    .then((lamports) => lamports / LAMPORTS_PER_SOL);
+    .then((lamports) => lamports / LAMPORTS_PER_SOL)
+    .then((balance) => {
+      console.log('GETTING MAIN ACCOUNT BALANCE', balance);
+      return balance;
+    });
 };
 
-export const stakeOnTask = (taskAccountPubKey: string, stakeAmount: number) =>
-  window.main.delegateStake({ taskAccountPubKey, stakeAmount });
+export const stakeOnTask = (taskAccountPubKey: string, stakeAmount: number) => {
+  console.log('STAKING ON', stakeAmount, taskAccountPubKey);
+  return window.main.delegateStake({ taskAccountPubKey, stakeAmount });
+};
 
 export const startTask = (taskAccountPubKey: string) => {
   console.log('STARTING TASK', taskAccountPubKey);
