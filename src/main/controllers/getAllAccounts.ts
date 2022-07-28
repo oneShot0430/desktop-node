7a0b6db04d8da69797942b827b00d0a4dc4c7711
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2022-07-28 19:38:58 +0500

    Added the isDefault flag and corrected pubkey retrieval

--- src/main/controllers/getAllAccounts.ts
+++ src/main/controllers/getAllAccounts.ts
@@ -2,11 +2,17 @@ import { Event } from 'electron';
 import fs from 'fs';
 import path from 'path';
 
+import { Keypair } from '@_koi/web3.js';
+
+import { namespaceInstance } from 'main/node/helpers/Namespace';
 import { getAllAccountsResponse } from 'models/api';
 
 import mainErrorHandler from '../../utils/mainErrorHandler';
 
-const getAllAccounts = (event: Event, payload: any): getAllAccountsResponse => {
+const getAllAccounts = async (
+  event: Event,
+  payload: any
+): Promise<getAllAccountsResponse> => {
   const mainWalletFiles = fs.readdirSync('wallets', { withFileTypes: true });
   const mainWalletfilesInDirectory = mainWalletFiles
     .filter((item) => item.isFile() && path.extname(item.name) === '.json')
@@ -18,22 +24,28 @@ const getAllAccounts = (event: Event, payload: any): getAllAccountsResponse => {
     .filter((item) => item.isFile() && path.extname(item.name) === '.json')
     .map((item) => item.name);
   const accounts: getAllAccountsResponse = [];
+  const activeAccount = await namespaceInstance.storeGet('ACTIVE_ACCOUNT');
+
   mainWalletfilesInDirectory.forEach((e) => {
-    const accountName = e.substring(0, e.lastIndexOf('_'));
-    const mainPublicKey = e.substring(e.lastIndexOf('_'), e.length - 5);
+    const currentAccountName = e.substring(0, e.lastIndexOf('_'));
+    const mainSystemWallet = Keypair.fromSecretKey(
+      Uint8Array.from(JSON.parse(fs.readFileSync(`wallets/${e}`, 'utf-8')))
+    );
     const stakingWalletNameArr = stakingWalletfilesInDirectory.filter(
-      (x) => x.substring(0, x.lastIndexOf('_')) == accountName
+      (x) => x.substring(0, x.lastIndexOf('_')) == currentAccountName
     );
     const stakingWalletName =
       stakingWalletNameArr.length > 0 ? stakingWalletNameArr[0] : '';
-    const stakingPublicKey = stakingWalletName.substring(
-      e.lastIndexOf('_'),
-      e.length - 5
+    const stakingWallet = Keypair.fromSecretKey(
+      Uint8Array.from(
+        JSON.parse(fs.readFileSync(`namespace/${stakingWalletName}`, 'utf-8'))
+      )
     );
     accounts.push({
-      accountName,
-      mainPublicKey,
-      stakingPublicKey,
+      accountName: currentAccountName,
+      mainPublicKey: mainSystemWallet.publicKey.toBase58(),
+      stakingPublicKey: stakingWallet.publicKey.toBase58(),
+      isDefault: activeAccount == currentAccountName,
     });
   });
   return accounts;
