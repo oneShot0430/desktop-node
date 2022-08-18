f9b567c6686625ac94866bc6e916c6fe0c291f1e
krzysztofjesien <krzysztof@koii.network>
2022-08-18 23:21:11 +0200

    Fix of load not working for initial items fitting in the table (no scroll)

--- src/webapp/components/ui/Table/InfiniteScrollTable.tsx
+++ src/webapp/components/ui/Table/InfiniteScrollTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { twMerge } from 'tailwind-merge';
 
@@ -23,22 +23,31 @@ export const InfiniteScrollTable = ({
   error,
   height,
 }: PropsType) => {
+  const tableWrapperId = 'infiniteTableWrapper';
   const tableClasses = twMerge(
     'h-[74vh] overflow-y-auto overflow-x-auto',
     height && `h-[${height}]`
   );
+
+  useEffect(() => {
+    const root = document.getElementById(tableWrapperId);
+    if (!isLoading && hasMore && root.scrollHeight <= root.clientHeight) {
+      update();
+    }
+  }, [isLoading, hasMore]);
 
   if (isLoading) return <div>Loading...</div>;
 
   if (error) return <div>{error}</div>;
 
   return (
-    <div className={tableClasses}>
+    <div className={tableClasses} id={tableWrapperId}>
       <InfiniteScroll
         dataLength={children.length}
         next={update}
         hasMore={hasMore}
         loader={<h4>Loading...</h4>}
+        scrollableTarget={tableWrapperId}
       >
         <table className="w-full text-[14px] text-left table-auto">
           <TableHeader tableHeaders={tableHeaders} />
