0dba476f3eda7b9eca46911c84e096a0aabd18c4
Wojciech Jelen <w.jelen20@gmail.com>
2022-09-22 23:16:01 +0200

    update

--- src/webapp/features/common/hooks/useRunMultipleTasks.ts
+++ src/webapp/features/common/hooks/useRunMultipleTasks.ts
@@ -13,19 +13,29 @@ export const useRunMultipleTasks = ({
   onRunAllTasksSuccessCallback,
 }: UseRunMultipleTasksParams) => {
   const handleRunTasks = async () => {
+    const errorMessages: string[] = [];
     const stakeOnTasksPromises = tasksToRun
       /**
        * @dev do not inlcude staking action promise if there is no stake
        */
       .filter(({ stake }) => stake > 0)
-      .map(({ stake, publicKey }) => stakeOnTask(publicKey, stake));
+      .map(({ stake, publicKey }) =>
+        stakeOnTask(publicKey, stake)
+          .then(() => {
+            return startTask(publicKey);
+          })
+          .catch((error) => {
+            const errorMessage = `Task ${publicKey} can't be debloyed because of error ${error}`;
+            console.log(errorMessage);
+            errorMessages.push(errorMessage);
+          })
+      );
 
-    /**
-     * @dev staking in each individual task before we continue
-     */
     await Promise.all(stakeOnTasksPromises);
-    const promises = tasksToRun.map(({ publicKey }) => startTask(publicKey));
-    await Promise.all(promises);
+
+    if (errorMessages.length > 0) {
+      throw errorMessages;
+    }
   };
 
   const {
