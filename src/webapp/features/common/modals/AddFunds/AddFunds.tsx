8a324e3fdba8b336563871b0899378250b66d09d
oneShot0430 <healer0991@gmail.com>
2022-10-21 13:24:19 -0300

    Make modal content dynamic based on faucet state

--- src/webapp/features/common/modals/AddFunds/AddFunds.tsx
+++ src/webapp/features/common/modals/AddFunds/AddFunds.tsx
@@ -6,9 +6,11 @@ import { useQuery } from 'react-query';
 import CurrencyIconSvg from 'assets/svgs/onboarding/currency-icon.svg';
 import CloseIconComponent from 'svgs/close-icons/close-icon-blue.svg';
 import { Button } from 'webapp/components';
+import { FAUCET_URL } from 'webapp/constants';
 import { useClipboard } from 'webapp/features/common';
 import { Modal, ModalContent } from 'webapp/features/modals';
-import { getMainAccountPublicKey } from 'webapp/services';
+import { getMainAccountPublicKey, getFaucetStatus } from 'webapp/services';
+import { ValidationStatus } from 'webapp/types';
 
 export const AddFunds = create(function AddFunds() {
   const modal = useModal();
@@ -17,9 +19,42 @@ export const AddFunds = create(function AddFunds() {
     ['main-account'],
     getMainAccountPublicKey
   );
+  // give faucetStatus an initial value so we can safely destructure it below
+  const { data: faucetStatus = { walletAddress: '' } } = useQuery(
+    ['faucet-status', mainAccountPubKey],
+    () => getFaucetStatus(mainAccountPubKey)
+  );
 
-  const openFaucet = () => {
-    window.open(`https://faucet.koii.live?key=${mainAccountPubKey}`, '_blank');
+  const { walletAddress: _, ...methods } = faucetStatus;
+  const methodsArray = Object.values(methods);
+  const methodsClaimed = methodsArray.reduce(
+    (accumulator, currentMethodStatus) =>
+      currentMethodStatus === ValidationStatus.CLAIMED
+        ? accumulator + 1
+        : accumulator,
+    0
+  );
+  const hasClaimedAllMethods = methodsClaimed === 4;
+  const title = hasClaimedAllMethods
+    ? 'Scan the QR code or copy the address to send tokens to your node account.'
+    : methodsClaimed === 1
+    ? 'Return to the Faucet to get the rest of your free KOII.'
+    : 'Go to the Faucet for some free KOII to get started.';
+
+  const openFaucetAndClose = () => {
+    window.open(
+      `${FAUCET_URL}${mainAccountPubKey}`,
+      '_blank',
+      'height=992,width=1512'
+    );
+    modal.resolve();
+    modal.remove();
+  };
+
+  const copyToClipboardAndClose = () => {
+    copyToClipboard(mainAccountPubKey);
+    modal.resolve();
+    modal.remove();
   };
 
   return (
@@ -37,35 +72,39 @@ export const AddFunds = create(function AddFunds() {
         </div>
 
         <div className="flex flex-col items-center w-full h-full">
-          <div className="mb-3 text-lg leading-8 text-center">
-            Go to the Faucet for some free KOII to get started.
-          </div>
+          <div className="mb-3 text-lg leading-8 text-center">{title}</div>
 
-          <Button
-            onClick={openFaucet}
-            label={'Get My Free Tokens'}
-            className="text-white bg-purple-4 w-[276px] h-[52px] mb-14 rounded-md"
-            icon={
-              <span className="w-[14px] h-[14px]">
-                <CurrencyIconSvg className="h-[100%]" />
-              </span>
-            }
-          />
+          {hasClaimedAllMethods ? (
+            <QRCode value="https://reactjs.org/" renderAs="canvas" size={240} />
+          ) : (
+            <>
+              <Button
+                onClick={openFaucetAndClose}
+                label={'Get My Free Tokens'}
+                className="text-white bg-purple-4 w-[276px] h-[52px] mb-14 rounded-md"
+                icon={
+                  <span className="w-[14px] h-[14px]">
+                    <CurrencyIconSvg className="h-[100%]" />
+                  </span>
+                }
+              />
 
-          <div className="mb-3">Or send KOII directly to this account.</div>
+              <div className="mb-3">Or send KOII directly to this account.</div>
 
-          <QRCode value="https://reactjs.org/" renderAs="canvas" size={80} />
+              <QRCode
+                value="https://reactjs.org/"
+                renderAs="canvas"
+                size={80}
+              />
+            </>
+          )}
 
           <div className="mt-4 mb-2 text-xs select-text">
             {mainAccountPubKey}
           </div>
 
           <Button
-            onClick={() => {
-              copyToClipboard(mainAccountPubKey);
-              modal.resolve();
-              modal.remove();
-            }}
+            onClick={copyToClipboardAndClose}
             label="copy"
             className="w-[72px] h-[24px] rounded-xl text-xs border border-finnieBlue bg-transparent text-finnieBlue-dark"
           />
---
+++ src/webapp/services/addFundsService.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+import { FAUCET_API_URL } from 'webapp/constants';
+import { StatusResponse } from 'webapp/types';
+
+export const getFaucetStatus = async (walletAddress: string) => {
+  const { data } = await axios.get<StatusResponse>(
+    `${FAUCET_API_URL}/get-user-faucet-state/${walletAddress}`
+  );
+  return data;
+};
