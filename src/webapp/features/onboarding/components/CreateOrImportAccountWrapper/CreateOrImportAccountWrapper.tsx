27aa6267405436dd254a085ed32f8d34e2acd562
oneShot0430 <healer0991@gmail.com>
2022-10-27 12:01:05 -0300

    Add missing bottom SVG shape to create/import acc screen

--- src/webapp/features/onboarding/components/CreateOrImportAccountWrapper/CreateOrImportAccountWrapper.tsx
+++ src/webapp/features/onboarding/components/CreateOrImportAccountWrapper/CreateOrImportAccountWrapper.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 
 import YellowLinesTop from 'assets/svgs/onboarding/key-import-lines-top.svg';
-// import TealLinesBottom from 'assets/svgs/onboarding/key-import-teal-lines-bot.svg';
+import TealLinesBottom from 'assets/svgs/onboarding/key-import-teal-lines-bot.svg';
 
 const CreateOrImportAccountWrapper = () => {
   const location = useLocation();
   const showBgShapes = !location.pathname.includes('backup');
 
   return (
-    <div className="relative h-full bg-finnieBlue-dark-secondary">
-      <div className="z-30 w-full h-full">
+    <div className="relative h-full bg-finnieBlue-dark-secondary overflow-hidden">
+      <div className="relative z-30 w-full h-full">
         <Outlet />
       </div>
 
       {showBgShapes && (
         <>
-          <YellowLinesTop className="absolute top-0 right-0 z-0" />
-          {/* <TealLinesBottom className="absolute bottom-0 left-0 z-[1]" /> */}
+          <YellowLinesTop className="absolute top-0 right-0" />
+          <TealLinesBottom className="absolute -bottom-2 -left-48" />
         </>
       )}
     </div>
