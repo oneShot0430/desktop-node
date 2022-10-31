edead79892427b3de5316771730faee0d7759b07
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2022-10-31 06:42:18 +0500

    Working on integrating new task structure in desktop node

--- src/main/type/TaskData.ts
+++ src/main/type/TaskData.ts
@@ -1,3 +1,7 @@
+import { ChildProcess } from 'child_process';
+
+import { Namespace } from 'main/node/helpers/Namespace';
+
 export interface TaskData {
   taskName: string;
   taskManager: string;
@@ -12,10 +16,17 @@ export interface TaskData {
   availableBalances: any;
   stakeList: Record<string, number>;
   isRunning: boolean;
-  cronArray: any;
 }
 
 export interface Task {
   publicKey: string;
   data: TaskData;
+}
+export interface IRunningTasks {
+  [key: string]: {
+    namespace: Namespace;
+    child: ChildProcess;
+    expressAppPort: number;
+    secret: string;
+  };
 }
