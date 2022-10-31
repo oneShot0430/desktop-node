5a5b5bda41cf8e7df24bd97ddbfc3a448c387272
oneShot0430 <healer0991@gmail.com>
2022-10-31 12:05:41 -0300

    Add proper action to Select more tasks button

--- src/webapp/features/onboarding/components/ConfirmYourStake/ConfirmYourStake.tsx
+++ src/webapp/features/onboarding/components/ConfirmYourStake/ConfirmYourStake.tsx
@@ -1,6 +1,6 @@
 import { sum } from 'lodash';
 import React, { useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import AddIconSvg from 'assets/svgs/onboarding/add-teal-icon.svg';
 import CurrencySvgIcon from 'assets/svgs/onboarding/currency-teal-small-icon.svg';
@@ -8,11 +8,13 @@ import { ErrorMessage } from 'webapp/components';
 import { Button } from 'webapp/components/ui/Button';
 import { useNotEnoughFunds, useRunMultipleTasks } from 'webapp/features/common';
 import { useMainAccountBalance } from 'webapp/features/settings';
+import { AppRoute } from 'webapp/routing/AppRoutes';
 import { TaskWithStake } from 'webapp/types';
 
 import { SelectedTasksSummary } from './SelectedTasksSummary';
 
 const ConfirmYourStake = () => {
+  const navigate = useNavigate();
   const { state: selectedTasks } = useLocation();
   const tasksToRun = selectedTasks as TaskWithStake[];
 
@@ -35,6 +37,8 @@ const ConfirmYourStake = () => {
     }
   };
 
+  const handleSelectMoreTasks = () => navigate(AppRoute.AddTask);
+
   return (
     <div className="relative h-full overflow-hidden bg-finnieBlue-dark-secondary">
       <div className="px-8">
@@ -46,6 +50,12 @@ const ConfirmYourStake = () => {
 
         <div className="flex justify-center mt-[40px]">
           <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-row items-center gap-2 mb-2 text-sm text-finnieEmerald-light">
+              <CurrencySvgIcon className="h-[24px]" />
+              {`Total balance: ${
+                isLoading ? 'Loading balance...' : balance
+              } KOII`}
+            </div>
             <Button
               className="font-semibold bg-finnieGray-light text-finnieBlue-light w-[220px] h-[38px]"
               label={runTasksLoading ? 'Running tasks...' : 'Confirm'}
@@ -57,12 +67,6 @@ const ConfirmYourStake = () => {
                 errorMessage={(runTasksError as { message: string }).message}
               />
             ) : null}
-            <div className="flex flex-row items-center gap-2 mt-2 text-sm text-finnieEmerald-light">
-              <CurrencySvgIcon className="h-[24px]" />
-              {`Total balance: ${
-                isLoading ? 'Loading balance...' : balance
-              } KOII`}
-            </div>
           </div>
         </div>
       </div>
@@ -72,6 +76,7 @@ const ConfirmYourStake = () => {
           label="Select more tasks"
           className="bg-transparent text-finnieEmerald-light w-max"
           icon={<AddIconSvg />}
+          onClick={handleSelectMoreTasks}
         />
       </div>
     </div>
