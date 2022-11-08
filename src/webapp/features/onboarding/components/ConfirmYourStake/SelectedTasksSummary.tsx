483c42ddfc697e07ddc9fe1c96870d4746c3c67c
oneShot0430 <healer0991@gmail.com>
2022-11-08 11:33:21 -0300

    Consider also tasks fees when verifying enough funds

--- src/webapp/features/onboarding/components/ConfirmYourStake/SelectedTasksSummary.tsx
+++ src/webapp/features/onboarding/components/ConfirmYourStake/SelectedTasksSummary.tsx
@@ -1,24 +1,25 @@
 import { sum } from 'lodash';
 import React, { useMemo } from 'react';
 
-import config from 'config';
 import { TaskWithStake } from 'webapp/types';
 
 import { TaskRow } from './TaskRow';
 
-const { TASK_FEE } = config.node;
-
 type PropsType = {
   selectedTasks: TaskWithStake[];
+  tasksFee: number;
 };
 
-export const SelectedTasksSummary = ({ selectedTasks }: PropsType) => {
+export const SelectedTasksSummary = ({
+  selectedTasks,
+  tasksFee,
+}: PropsType) => {
   const listEmpty = selectedTasks.length === 0;
-  const taskFees = `~ ${TASK_FEE * selectedTasks.length}`;
   const totalKoiiStaked = useMemo(
     () => sum(selectedTasks.map((task) => task.stake)),
     [selectedTasks]
   );
+  const tasksFeeToDisplay = `~ ${tasksFee}`;
 
   // if (listEmpty) {
   //   return null;
@@ -46,7 +47,7 @@ export const SelectedTasksSummary = ({ selectedTasks }: PropsType) => {
       <div className="flex flex-row w-full text-lg text-finnieEmerald-light px-12">
         <div className="w-[70%]">
           <div className="mb-1 font-semibold text-finnieOrange">Task Fees</div>
-          <div className="text-white">{taskFees} KOII</div>
+          <div className="text-white">{tasksFeeToDisplay} KOII</div>
         </div>
         <div className="w-[30%]">
           <div className="mb-2 font-semibold text-finnieEmerald-light">
