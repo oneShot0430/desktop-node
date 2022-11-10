9d5af3105f6cfcaeec4b93a6ae453e572aa533a8
oneShot0430 <healer0991@gmail.com>
2022-11-10 19:41:21 -0300

    Add pin input

--- src/webapp/components/Unlock/Unlock.tsx
+++ src/webapp/components/Unlock/Unlock.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import KoiiLogo from 'assets/svgs/koii-logo-white.svg';
 import WelcomeLinesDiagonal from 'assets/svgs/welcome-lines-diagonal.svg';
 import WelcomeWheelBackground from 'assets/svgs/welcome-wheel-background.svg';
+import { PinInput } from 'webapp/components/PinInput';
 
 export const Unlock = (): JSX.Element => {
+  const handlePinChange = (pin: string) => {
+    console.log('pin: ', pin);
+  };
+
   return (
     <div className="relative h-full overflow-y-auto flex flex-col justify-center items-center gap-5 h-full bg-gradient-to-b from-finnieBlue-dark-secondary to-finnieBlue text-white">
       <WelcomeWheelBackground className="absolute top-0 left-0 h-[40%]" />
@@ -15,6 +20,8 @@ export const Unlock = (): JSX.Element => {
       <p className="text-lg text-center">
         Enter your security PIN code to unlock your Node
       </p>
+
+      <PinInput onChange={handlePinChange} showHideButton={false} />
 
       <WelcomeLinesDiagonal className="absolute bottom-0 right-0 h-full" />
     </div>
