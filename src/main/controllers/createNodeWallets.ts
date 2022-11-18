9e73951d3e5fae86e17d7272ba73d80a6404c14f
oneShot0430 <healer0991@gmail.com>
2022-11-18 17:24:00 -0300

    Update account creation errors

--- src/main/controllers/createNodeWallets.ts
+++ src/main/controllers/createNodeWallets.ts
@@ -5,7 +5,9 @@ import { Keypair } from '@_koi/web3.js';
 import * as bip39 from 'bip39';
 import { derivePath } from 'ed25519-hd-key';
 
+import { ErrorType } from 'models';
 import { CreateNodeWalletsParam, CreateNodeWalletsResponse } from 'models/api';
+import { DetailedError } from 'utils';
 
 import mainErrorHandler from '../../utils/mainErrorHandler';
 import { getAppDataPath } from '../node/helpers/getAppDataPath';
@@ -17,10 +19,18 @@ const createNodeWallets = async (
   console.log('IN CREATE WALLET  API');
   const { mnemonic, accountName } = payload;
   if (!mnemonic) {
-    throw new Error('Please provide mnemonic to generate wallets');
+    throw new DetailedError({
+      detailed: `Please provide mnemonic to generate wallets, got ${mnemonic}`,
+      summary: 'Please provide mnemonic to generate wallets',
+      type: ErrorType.NO_MNEMONIC,
+    });
   }
   if (!accountName) {
-    throw new Error('Please provide accountName to generate wallets');
+    throw new DetailedError({
+      detailed: `Please provide accountName to generate wallets, got ${accountName}`,
+      summary: 'Please provide accountName to generate wallets',
+      type: ErrorType.NO_VALID_ACCOUNT_NAME,
+    });
   }
   if (!fs.existsSync(getAppDataPath() + '/namespace'))
     fs.mkdirSync(getAppDataPath() + '/namespace');
@@ -28,14 +38,22 @@ const createNodeWallets = async (
     fs.mkdirSync(getAppDataPath() + '/wallets');
 
   if (!/^[0-9a-zA-Z ... ]+$/.test(accountName)) {
-    throw new Error('Please provide a valid accountName');
+    throw new DetailedError({
+      detailed: `Please provide a valid accountName, got ${accountName}`,
+      summary: 'Please provide a valid accountName',
+      type: ErrorType.NO_VALID_ACCOUNT_NAME,
+    });
   }
   try {
     // Creating stakingWallet
     const stakingWalletFilePath =
       getAppDataPath() + `/namespace/${accountName}_stakingWallet.json`;
     if (fs.existsSync(stakingWalletFilePath)) {
-      throw new Error('Staking wallet with same account name already exists');
+      throw new DetailedError({
+        detailed: `Staking wallet with same account name ("${accountName}") already exists`,
+        summary: 'Staking wallet with same account name already exists',
+        type: ErrorType.NO_VALID_ACCOUNT_NAME,
+      });
     }
     console.log('WALLET PATH', stakingWalletFilePath);
     const stakingSeed = bip39.mnemonicToSeedSync(mnemonic, '');
@@ -59,7 +77,11 @@ const createNodeWallets = async (
     const mainWalletFilePath =
       getAppDataPath() + `/wallets/${accountName}_mainSystemWallet.json`;
     if (fs.existsSync(mainWalletFilePath)) {
-      throw new Error('Main wallet with same account name already exists');
+      throw new DetailedError({
+        detailed: `Main wallet with same account name ("${accountName}") already exists`,
+        summary: 'Main wallet with same account name already exists',
+        type: ErrorType.NO_VALID_ACCOUNT_NAME,
+      });
     }
     console.log('WALLET PATH', mainWalletFilePath);
     const mainSeed = bip39.mnemonicToSeedSync(mnemonic, '');
