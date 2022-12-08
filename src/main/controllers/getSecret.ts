c020e1e2663046e73d9fba6aa3eb40c88298ebb7
somali0128 <ldydq970128@gmail.com>
2022-12-08 17:35:13 -0400

    add TODO: fetch source code

--- src/main/controllers/getSecret.ts
+++ src/main/controllers/getSecret.ts
@@ -6,9 +6,15 @@ import koiiTasks from 'services/koiiTasks';
 
 import mainErrorHandler from '../../utils/mainErrorHandler';
 
+import getTaskSource from './getTaskSource';
+
 const getSecret = (event: Event, payload: GetSecretParam): Task[] => {
   const { tasksIds } = payload || {};
   const response = tasksIds.map((e) => koiiTasks.getTaskByPublicKey(e));
+  getTaskSource(response);
+  /*
+   *TODO: review getTaskSourceCode() then fetch the source code
+   */
   return response;
 };
 
