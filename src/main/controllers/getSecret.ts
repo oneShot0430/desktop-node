729bfb3fcdb8021020d1d3e45327d2863917083a
somali0128 <ldydq970128@gmail.com>
2022-12-13 16:50:14 -0400

    udpate

--- src/main/controllers/getSecret.ts
+++ src/main/controllers/getSecret.ts
@@ -1,21 +1,16 @@
 import { Event } from 'electron';
 
-import { Task } from 'models';
 import { GetSecretParam } from 'models/api';
-import koiiTasks from 'services/koiiTasks';
 
 import mainErrorHandler from '../../utils/mainErrorHandler';
 
-import getTaskSource from './getTaskSource';
-
-const getSecret = (event: Event, payload: GetSecretParam): Task[] => {
-  const { tasksIds } = payload || {};
-  const response = tasksIds.map((e) => koiiTasks.getTaskByPublicKey(e));
-  getTaskSource(response);
+const getSecret = async (
+  event: Event,
+  payload: GetSecretParam
+): Promise<GetSecretParam> => {
   /*
    *TODO: review getTaskSourceCode() then fetch the source code
    */
-  return response;
 };
 
 export default mainErrorHandler(getSecret);
