402a2b4798299a78e708103cbe7b5f786090ef3c
Wojciech Jelen <w.jelen20@gmail.com>
2022-12-14 14:20:43 +0100

    Added some basic tests

---
+++ src/webapp/components/ui/Dropdown/Dropdown.spec.tsx
@@ -0,0 +1,42 @@
+import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
+
+import { Dropdown } from './Dropdown';
+
+const items = [
+  { label: 'First item', disabled: false },
+  { label: 'Second item', disabled: true },
+  { label: 'Third item', disabled: false },
+];
+
+describe('Dropdown', () => {
+  it('should render without crashing', () => {
+    const { container } = render(<Dropdown items={items} />);
+    expect(container).toBeTruthy();
+  });
+
+  it('should display placeholder text when no item is selected', () => {
+    const { getByText } = render(<Dropdown items={items} />);
+    expect(getByText('Select item')).toBeInTheDocument();
+  });
+
+  it('should display the selected item text', () => {
+    const { getByText } = render(<Dropdown items={items} />);
+    fireEvent.click(getByText('Select item'));
+    fireEvent.click(getByText('First item'));
+    expect(getByText('First item')).toBeInTheDocument();
+  });
+
+  it('should call the onSelect callback when an item is selected', () => {
+    const onSelect = jest.fn();
+    const { getByText } = render(
+      <Dropdown items={items} onSelect={onSelect} />
+    );
+    fireEvent.click(getByText('Select item'));
+    fireEvent.click(getByText('First item'));
+    expect(onSelect).toHaveBeenCalledWith({
+      label: 'First item',
+      disabled: false,
+    });
+  });
+});
