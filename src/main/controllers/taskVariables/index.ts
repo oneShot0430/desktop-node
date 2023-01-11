13f99b81065aedf4d9a292722bf89cacdf724ad8
krzysztofjesien <krzysztof@koii.network>
2023-01-11 22:19:05 +0100

    Pair Task Variable Endpoint with controller and unit tests

--- src/main/controllers/taskVariables/index.ts
+++ src/main/controllers/taskVariables/index.ts
@@ -3,3 +3,5 @@ export * from './storeTaskVariable';
 export * from './getTaskVariablesNames';
 export * from './editTaskVariable';
 export * from './deleteTaskVariable';
+export * from './getStoredPairedTaskVariables';
+export * from './pairTaskVariable';
