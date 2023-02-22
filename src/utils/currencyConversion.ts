e9ab0c577a412b94726bc13f0f8a41b31006ee9f
krzysztofjesien <krzysztof@koii.network>
2023-02-22 23:08:46 +0100

    temp solution to use of native deps in renderer

--- src/utils/currencyConversion.ts
+++ src/utils/currencyConversion.ts
@@ -1,4 +1,6 @@
-import { LAMPORTS_PER_SOL as ROE_PER_KOII } from '@_koi/web3.js';
+// import { LAMPORTS_PER_SOL as ROE_PER_KOII } from '@_koi/web3.js';
+
+const ROE_PER_KOII = 1000000000;
 
 export const getKoiiFromRoe = (roe: number) => roe / ROE_PER_KOII;
 
