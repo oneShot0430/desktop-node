7b4fecac902fc1bccdabc0cfa2a06b7cf727e357
krzysztofjesien <krzysztof@koii.network>
2023-02-23 22:13:55 +0100

    webpack config update

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -9,8 +9,6 @@ import { dependencies as externals } from '../../release/app/package.json';
 
 import webpackPaths from './webpack.paths';
 
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-
 const configuration: webpack.Configuration = {
   externals: [...Object.keys(externals || {})],
 
@@ -62,17 +60,6 @@ const configuration: webpack.Configuration = {
       NODE_ENV: 'production',
     }),
     new ForkTsCheckerWebpackPlugin(),
-    new CircularDependencyPlugin({
-      // exclude detection of files based on a RegExp
-      exclude: /a\.js|node_modules/,
-      // add errors to webpack instead of warnings
-      failOnError: true,
-      // allow import cycles that include an asyncronous import,
-      // e.g. via import(/* webpackMode: "weak" */ './file.js')
-      allowAsyncCycles: false,
-      // set the current working directory for displaying module paths
-      cwd: process.cwd(),
-    }),
   ],
 };
 
