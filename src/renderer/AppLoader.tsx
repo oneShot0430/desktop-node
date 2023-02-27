a538488b7f38eec5693414507ad65832f24d9ae3
oneShot0430 <healer0991@gmail.com>
2023-02-27 17:15:03 -0300

    Add spinner to AppLoader

--- src/renderer/AppLoader.tsx
+++ src/renderer/AppLoader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
+import { LoadingSpinner } from 'renderer/components';
 import { AppRoute } from 'renderer/types/routes';
 
 import { useUserSettings } from './features/common';
@@ -8,12 +9,13 @@ import { useUserSettings } from './features/common';
 function AppLoader(): JSX.Element {
   const { settings, loadingSettings } = useUserSettings();
 
-  if (loadingSettings) return <div>loading...</div>;
+  const routeToNavigate = settings?.onboardingCompleted
+    ? AppRoute.Unlock
+    : AppRoute.OnboardingCreatePin;
 
-  if (settings?.onboardingCompleted) {
-    return <Navigate to={AppRoute.Unlock} />;
-  }
-  return <Navigate to={AppRoute.OnboardingCreatePin} />;
+  if (loadingSettings) return <LoadingSpinner className="m-auto" />;
+
+  return <Navigate to={routeToNavigate} />;
 }
 
 export default AppLoader;
