fece270c23dd7557f9b1a2073b0f6e3e467d78af
oneShot0430 <healer0991@gmail.com>
2023-03-28 22:49:54 -0300

    Extract logic to get formatted date to util getCreatedAtDate

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,12 @@
 import { getKoiiFromRoe, getRoeFromKoii } from './currencyConversion';
 import { throwDetailedError } from './error';
+import { getCreatedAtDate } from './getCreatedAtDate';
 import mainErrorHandler from './mainErrorHandler';
 
-export { mainErrorHandler, getKoiiFromRoe, getRoeFromKoii, throwDetailedError };
+export {
+  mainErrorHandler,
+  getKoiiFromRoe,
+  getRoeFromKoii,
+  throwDetailedError,
+  getCreatedAtDate,
+};
