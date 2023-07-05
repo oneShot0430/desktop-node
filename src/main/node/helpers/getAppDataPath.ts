7b5004eeb4c4655ad99286ff369499adf4cef82c
Arthur Morgan <arthur@koii.network>
2023-07-05 16:16:10 +0000

    Resolve "Allow option to create config files (wallets, exacutables, etc.) locally for dev runs only."

--- src/main/node/helpers/getAppDataPath.ts
+++ src/main/node/helpers/getAppDataPath.ts
@@ -5,6 +5,11 @@ const platformsSupported = ['darwin', 'win32', 'linux'];
 
 // eslint-disable-next-line consistent-return
 export function getAppDataPath() {
+  if (
+    process.env.DB_MODE === 'development' &&
+    process.env.NODE_ENV === 'development'
+  )
+    return './appdata';
   if (platformsSupported.includes(process.platform))
     return path.join(app.getPath('appData'), 'KOII-Desktop-Node');
   else {
