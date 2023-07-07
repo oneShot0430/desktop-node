8e6c552a8104287726eedee5bb93f2ba7ba439fd
Krzysztof Jesień <krzysztof@koii.network>
2023-07-07 12:35:59 +0000

    Auto updater cache path overwrite, reenable, allow downgrade and disable auto install on quit

--- src/main/AppUpdater.ts
+++ src/main/AppUpdater.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow, dialog } from 'electron';
+import path from 'path';
 
 import log from 'electron-log';
 import { autoUpdater } from 'electron-updater';
@@ -6,6 +7,7 @@ import { autoUpdater } from 'electron-updater';
 import { RendererEndpoints } from 'config/endpoints';
 
 import { getUserConfig } from './controllers';
+import { getAppDataPath } from './node/helpers/getAppDataPath';
 
 const CHECK_INTERVAL = 6 * 1000 * 60 * 60;
 
@@ -22,9 +24,22 @@ export async function initializeAppUpdater(
 async function configureUpdater() {
   autoUpdater.logger = log;
   autoUpdater.autoDownload = false;
+  autoUpdater.allowDowngrade = true;
+  autoUpdater.autoInstallOnAppQuit = false;
 
-  const userConfig = await getUserConfig();
-  autoUpdater.channel = userConfig?.alphaUpdatesEnabled ? 'alpha' : 'latest';
+  console.log(
+    'original updater cache path ',
+    (autoUpdater as any).app.baseCachePath
+  );
+  Object.defineProperty((autoUpdater as any).app, 'baseCachePath', {
+    get() {
+      return path.join(getAppDataPath(), 'updater-cache');
+    },
+  });
+  console.log(
+    'overwritten updater cache path ',
+    (autoUpdater as any).app.baseCachePath
+  );
 }
 
 function setListeners(mainWindow: BrowserWindow | undefined) {
@@ -65,12 +80,12 @@ function setListeners(mainWindow: BrowserWindow | undefined) {
       dialog
         .showMessageBox({
           type: 'question',
-          buttons: ['Restart & Update', 'Update Later'],
+          title: 'Koii Desktop Node',
+          buttons: ['Update and Restart', 'Update Later'],
           defaultId: 0,
-          message:
-            'Get the latest update. Do you want to restart and update now?',
+          message: 'Do you want to update and restart now?',
+          detail: `Get the latest version: ${info.version}`,
         })
-        // eslint-disable-next-line promise/no-nesting
         .then((selection) => {
           if (selection.response === 0) {
             // User clicked 'Restart & Update'
@@ -84,12 +99,14 @@ function setListeners(mainWindow: BrowserWindow | undefined) {
 function createCheckForTheUpdatesInterval() {
   if (!interval) {
     interval = setInterval(() => {
+      console.log('interval update check');
       autoUpdater.checkForUpdates();
     }, CHECK_INTERVAL);
   }
 
   // runs the first check 25sec after the app initialization
   setTimeout(() => {
+    console.log('initial update check');
     autoUpdater.checkForUpdates();
   }, 25000);
 }
